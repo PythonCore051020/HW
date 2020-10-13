4b332fbc05c5428b834b4befe250c3cc2bc1208d
lhalam <galamagal86@gmail.com>
2020-10-13 20:35:03 +0300

    add lesson 03

---
+++ lessons/lesson01/example.py
@@ -0,0 +1,87 @@
+# MY_INT = 25
+# cs1_1 = 1
+
+
+# bool1 = True
+# print(bool1, id(bool1))
+# bool1 = 0
+# print(bool1, id(bool1))
+# bool1 = "bdjhf gvjfd"
+# print(bool1, id(bool1))
+# bool1 = lambda a: a**2
+# print(bool1(10), id(bool1))
+# bool1 = []
+# print(bool1, id(bool1))
+# bool1.append(1)
+# print(bool1, id(bool1))
+# bool1.append(1)
+# print(bool1, id(bool1))
+# print()
+# bool1 = 0
+# print(bool1, id(bool1))
+# bool1 +=1
+# print(bool1, id(bool1))
+# bool1 +=1
+# print(bool1, id(bool1))
+
+
+# i = 99**99**2
+# print(len(str(i)), i)
+# print(int("345"))
+# print(int("345", 8))
+# print(int("345", 16))
+# print(int("010101010111", 2))
+# print(int("010101010111", 36))
+# print(int("100", 3))
+# print(int("100", 4))
+# print(int("z", 36))
+# print(int("10", 36))
+
+
+# print(float("11.1"))
+# print(float("a11,1"))
+
+# a = 4
+# b = 3
+# c = eval("a*b")
+# print(c)
+
+# for i in range (255):
+#     print(f"{i} - {chr(i)}")
+# print(ord('a'))
+
+# a = 1
+# print(type(a))
+# a = "1"
+# print(type(a))
+# a = .1
+# print(type(a))
+# print(type(a) is float )
+# a = 1
+# print(type(a) is float )
+# print(isinstance(a, int))
+
+# a = "str"
+# for i in a:
+#     print(i)
+#
+# a = [10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
+# print(a[0])
+# print(a[-0])
+# print(a[-1])
+# print(a[-2])
+# print(a[1: 5])
+# print(a[1: 5: 2])
+# print(a[: 5: 2])
+# print(a[1:: 2])
+# print(a[:: 2])
+
+
+# a = "()"
+# print(dir())
+# print(dir(a))
+# print([i for i in dir(a) if not i[0] == "_"])
+# help(str.find)
+
+
+print((-1)**0.5)
