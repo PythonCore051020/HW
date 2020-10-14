e2e4ae054b232e2a23f3a3ed61cf796cac8a68c8
Ivan <lvwwvl.i@gmail.com>
2020-10-14 22:31:00 +0300

    add HW01

---
+++ HW01/ivan_l/task 1.py
@@ -0,0 +1,18 @@
+Python 3.6.9 (default, Jul 17 2020, 12:50:27) 
+[GCC 8.4.0] on linux
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> a = 9
+>>> b = 7
+>>> print(a)
+9
+>>> print(b)
+7
+>>> a + b
+16
+>>> a - b
+2
+>>> a * b
+63
+>>> a / b
+1.2857142857142858
+>>> 
