9605766b5ebcdee00db20116691306f846c1a4f7
Paliei Serhii <Seregapaley@gmail.com>
2020-10-15 11:18:38 +0300

    Re_Home work

---
+++ HW01/Spali/Task2.py
@@ -0,0 +1,6 @@
+
+nameuser=input(("What is your name?:"))
+olduser=input(("How old are you?:"))
+live=input(("Where do you live?:"))
+print("Hello",nameuser,".","Your age is",olduser,".","You live in",live,".")
+
