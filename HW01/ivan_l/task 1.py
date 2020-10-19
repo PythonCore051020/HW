5d75479109d59313e8e3f72fa5d675cde31ee1d2
ivannle <72571447+ivannle@users.noreply.github.com>
2020-10-19 21:43:58 +0300

    Update task 1.py

--- HW01/ivan_l/task 1.py
+++ HW01/ivan_l/task 1.py
@@ -1,18 +1,15 @@
-Python 3.6.9 (default, Jul 17 2020, 12:50:27) 
-[GCC 8.4.0] on linux
-Type "help", "copyright", "credits" or "license()" for more information.
->>> a = 9
->>> b = 7
->>> print(a)
+a = 9
+b = 7
+print(a)
 9
->>> print(b)
+print(b)
 7
->>> a + b
+a + b
 16
->>> a - b
+a - b
 2
->>> a * b
+a * b
 63
->>> a / b
+a / b
 1.2857142857142858
->>> 
+
