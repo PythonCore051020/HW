a4b56ebaca8c6a92db4c392c519ab1cd654c3048
ivannle <72571447+ivannle@users.noreply.github.com>
2020-10-19 21:45:06 +0300

    Update task 2.py

--- HW01/ivan_l/task 2.py
+++ HW01/ivan_l/task 2.py
@@ -1,16 +1,13 @@
-Python 3.6.9 (default, Jul 17 2020, 12:50:27) 
-[GCC 8.4.0] on linux
-Type "help", "copyright", "credits" or "license()" for more information.
->>> q1 = input('What is your name? My name is ')
+q1 = input('What is your name? My name is ')
 What is your name? My name is Ivan
->>> print ('Hello',q1)
+print ('Hello',q1)
 Hello Ivan
->>> q2 = input('How old are you? ')
+q2 = input('How old are you? ')
 How old are you? 20
->>> print ('Your age is',q2)
+print ('Your age is',q2)
 Your age is 20
->>> q3 = input('Where do you live? ')
+q3 = input('Where do you live? ')
 Where do you live? Lviv
->>> print ('You live in',q3)
+print ('You live in',q3)
 You live in Lviv
->>> 
+
