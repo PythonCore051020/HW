64a604d5a6d61695bc0542efc6b681a1abd3b5e1
lhalam <galamagal86@gmail.com>
2020-10-29 21:05:23 +0200

    added lesson08

---
+++ lessons/lesson08/ex2.py
@@ -0,0 +1,14 @@
+# print(__name__)
+# print(__file__)
+# print(__package__)
+__all__ = ["CONST", "f"]
+CONST = 10
+CONSTS = [1, 2, 3]
+
+def f():
+    print(CONST, CONSTS)
+# import ex1
+# import ex1
+# print(__name__)
+# if __name__ == "__main__":
+#     print("temp2")
