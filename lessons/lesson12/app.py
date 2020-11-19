d573b7a0eeba2b1aa25cdd6fb1c367ad0b8ef1ce
lhalam <galamagal86@gmail.com>
2020-11-19 20:39:53 +0200

    added lesson 13

--- lessons/lesson12/app.py
+++ lessons/lesson12/app.py
@@ -13,6 +13,9 @@ class City(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     size = db.Column(db.Integer)
+    country_id = db.Column(db.Integer, db.ForeignKey('country.id'), nullable=True)
+    country = db.relationship('Country', backref=db.backref('cities', lazy=True))
+
     def __repr__(self):
         return f"<{self.id} {self.name} {self.size}>"
 
@@ -46,6 +49,43 @@ class City(db.Model):
     def delete_by_id(pk):
         city = City.get_by_id(pk)
         db.session.delete(city)
+        db.session.commit()
+
+
+class Country(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+
+    def __repr__(self):
+        return f"<{self.id} {self.name}>"
+
+    @staticmethod
+    def create(name):
+        country = Country()
+        try:
+            country.name = str(name)
+        except ValueError:
+            return None
+        db.session.add(country)
+        db.session.commit()
+        return country
+
+    @staticmethod
+    def get_by_id(pk):
+        country = Country.query.filter_by(id=pk).first()
+        return country
+
+    @staticmethod
+    def update(pk, name=None):
+        country = Country.get_by_id(pk)
+        if not name == None:
+            country.name = name
+        db.session.commit()
+
+    @staticmethod
+    def delete_by_id(pk):
+        country = Country.get_by_id(pk)
+        db.session.delete(country)
         db.session.commit()
 
 
@@ -83,7 +123,24 @@ def city():
 @app.route('/city/list')
 def city_list():
     cities = City.query.all()
+    # countries = Country.query.all()
+    # date = []
+    # for city in cities:
+    #     country_name = None
+    #     for country in countries:
+    #         if country.id == city.country_id:
+    #             country_name = country.name
+    #     element = {
+    #         "id": city.id,
+    #         "name": city.name,
+    #         "size": city.size,
+    #         "country": country_name,
+    #         "country_id": city.country_id
+    #     }
+    #     date.append(element)
+    # return render_template('cities.html', date=date)
     return render_template('cities.html', date=cities)
+
 
 @app.route('/city/<pk>')
 def city_by_id(pk):
@@ -91,7 +148,8 @@ def city_by_id(pk):
     if city:
         return render_template('city.html', date=city)
 
-@app.route('/city/update/<pk>',  methods=['GET', 'POST'])
+
+@app.route('/city/update/<pk>', methods=['GET', 'POST'])
 def city_update_by_id(pk):
     if request.method == 'GET':
         city = City.get_by_id(pk)
@@ -100,16 +158,32 @@ def city_update_by_id(pk):
         name = request.form['city']
         size = request.form['size']
         City.update(pk, name=name, size=size)
-        return redirect('/city/list')\
-
+        return redirect('/city/list') \
+ \
+ \
 @app.route('/city/delete/<pk>')
 def city_delete_by_id(pk):
     City.delete_by_id(pk)
     return redirect('/city/list')
 
+
 @app.route('/user/template')
 def user_template():
     return render_template('hello.html', name="aaaa", bbbb=2222, date={"name": "Anna", "age": 22})
+
+
+@app.route('/country/list')
+def country_list():
+    countries = Country.query.all()
+    return render_template('country/countries_list.html', countries=countries)
+
+
+@app.route('/country/<pk>')
+def country_by_id(pk):
+    country = Country.get_by_id(pk)
+    # cityes = [city for city in City.query.all() if  city.country_id == country.id]
+    if country:
+        return render_template('country/country.html', country=country)
 
 
 #
@@ -121,7 +195,8 @@ def user_template():
 #
 
 if __name__ == '__main__':
-    # db.create_all()
+    db.create_all()
+
     # c1 = City(name="Lviv", size=890_000)
     # c2 = City(name="Odesa", size="1_890_000")
     # db.session.add(c1)
